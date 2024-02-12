9a830c51d6b5b580bcbf6971368490537bb31aa5
maulibrahim20 <maulibrahim20@student.polindra.ac.id>
2024-02-12 12:30:04 +0700

    Add SweetAlert for success and error messages in JabatanKecamatanController  CRUD and update index.blade.php

--- app/Http/Controllers/WEB/AdminKec/Master/JabatanKecamatanController.php
+++ app/Http/Controllers/WEB/AdminKec/Master/JabatanKecamatanController.php
@@ -7,7 +7,7 @@ use App\Models\AdminKec\JabatanKecamatan;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Carbon\Carbon;
-
+use RealRashid\SweetAlert\Facades\Alert;
 
 class JabatanKecamatanController extends Controller
 {
@@ -34,25 +34,55 @@ class JabatanKecamatanController extends Controller
                 'user_id' => auth()->user()->id,
             ]);
             DB::commit();
+
+            Alert::success('Berhasil', 'Berhasil Menambahkan Jabatan');
             return redirect()->back()->with('success', 'Berhasil menambahkan jabatan');
         } catch (\Throwable $th) {
             DB::rollBack();
+            Alert::error('Gagal', 'Gagal Update Jabatan' . $th->getMessage());
             return redirect()->back()->with('error', 'Gagal menambahkan jabatan');
         }
     }
 
     public function update(Request $request, $id)
     {
-        dd($request->all());
         try {
+            DB::beginTransaction();
             $jabatan = $this->jabatan::findOrFail($id);
             $jabatan->update([
                 'name' => $request->name,
                 'updated_at' => Carbon::now(),
                 'user_id' => auth()->user()->id,
             ]);
+
+            DB::commit();
+
+            Alert::success('Berhasil', 'Berhasil Mengupdate Jabatan');
+            return back()->with('success', 'Berhasil Update Jabatan');
         } catch (\Exception $e) {
-            return back()->with('error', 'Gagal Update Jabatan');
+
+            DB::rollback();
+            Alert::error('Gagal', 'Gagal Update Jabatan' . $e->getMessage());
+            return back()->with('error', 'Gagal Update Jabatan' . $e->getMessage());
+        }
+    }
+
+    public function destroy($id)
+    {
+        try {
+            DB::beginTransaction();
+            $jabatan = $this->jabatan::findOrFail($id);
+            $jabatan->delete();
+
+            DB::commit();
+
+            Alert::success('Berhasil', 'Data Berhasil Dihapus');
+            return back()->with('success', 'Data Berhasil Dihapus!');
+        } catch (\Exception $e) {
+            DB::rollback();
+
+            Alert::error('Gagal', 'Data Gagal Dihapus Terjadi kesahalahn' . $e->getMessage());
+            return back()->with('error', 'Data GagalDihapus' . $e->getMessage());
         }
     }
 }
--- resources/views/admin_kecamatan/pages/master/jabatan/index.blade.php
+++ resources/views/admin_kecamatan/pages/master/jabatan/index.blade.php
@@ -40,10 +40,11 @@
                                         <td><strong>{{ $loop->iteration }}</strong></td>
                                         <td>{{ $data->name }}</td>
                                         <td class="text-center">
-                                            <a href="{{ url('/admin/kec/master/jabatan/' . $data->id . '/edit') }}"
-                                                class="btn btn-icon btn-warning"><i class="fe fe-edit"></i></a>
+                                            <a class="btn btn-icon btn-warning" data-bs-toggle="modal"
+                                                data-bs-target="#EditModal{{ $data->id }}"><i
+                                                    class="fe fe-edit"></i></a>
                                             <form style="display: inline;"
-                                                action="{{ url('/admin/kab/permssion/role/' . $data->id) }}" method="post">
+                                                action="{{ url('/admin/kec/master/jabatan/' . $data->id) }}" method="post">
                                                 @method('DELETE')
                                                 @csrf
                                                 <button type="submit" data-id="{{ $data->id }}"
@@ -94,4 +95,70 @@
         </div>
     </div>
     {{-- end modal tambah --}}
+
+    {{-- start modal edit --}}
+    @foreach ($jabatan as $edit)
+        <div class="modal fade" id="EditModal{{ $edit->id }}">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">
+                            Edit Data
+                        </h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <form class="needs-validation was-validated" method="POST"
+                        action="{{ url('/admin/kec/master/jabatan/' . $edit->id) }}">
+                        @method('PUT')
+                        @csrf <!-- Fungsi Pengamanan -->
+                        <div class="modal-body">
+                            <div class="col-lg-12">
+                                <div class="form-group">
+                                    <label class="form-label">Nama Jabatan</label>
+                                    <input class="form-control mb-4" placeholder="Masukan Nama Role" required=""
+                                        type="text" name="name" value="{{ $edit->name }}">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="submit" class="btn btn-primary">Save changes</button>
+                            <button class="btn btn-light" data-bs-dismiss="modal">Close</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    @endforeach
+
+    {{-- end modal edit --}}
+@endsection
+
+@section('script')
+    <script>
+        // Menangkap klik tombol hapus
+        $('.deleteBtn').on('click', function(e) {
+            e.preventDefault();
+            var id = $(this).data('id');
+
+            // Tampilkan dialog konfirmasi
+            Swal.fire({
+                title: 'Anda yakin ingin menghapus?',
+                text: "Tindakan ini tidak dapat dibatalkan!",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#d33',
+                cancelButtonColor: '#3085d6',
+                confirmButtonText: 'Ya, hapus!',
+                cancelButtonText: 'Batal'
+            }).then((result) => {
+                // Jika pengguna menekan tombol "Ya, hapus!"
+                if (result.isConfirmed) {
+                    // Kirimkan form untuk menghapus data
+                    $(this).closest('form').submit();
+                }
+            });
+        });
+    </script>
 @endsection
